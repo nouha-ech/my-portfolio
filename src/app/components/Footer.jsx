58584bcd61d23c13e7aa880c73baf79515436fe4
nouhaila echchnaoui <nouhaila.chan2001@gmail.com>
2024-08-18 09:54:59 +0100

    r

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
         <span>N.E</span>
-        <p className="text-slate-600">© 2024 Nohaila Ech.chnaoui.</p>
+        <p className="text-slate-600">© 2024 Nohaila Ech.chnaoui</p>
       </div>
     </footer>
   );
